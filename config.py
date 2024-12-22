1fafaa55898ef37770a40351458ee3570bb473cd
paidbotXtitan01 <paidbotop24+1@outlook.com>
2024-12-22 14:50:19 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "7788865701:AAEHVM8BGHCY_fIECUSn_ydJ_45Jn0p72-M"
+ADMIN_IDS = ["7163028849"]
+GROUP_ID = -1002421977380  # Replace with your group ID
+GROUP_LINK = "https://t.me/+8Uy26VLnS3A2ZTg1"  # Replace with your group invite link
+# Default number of threads for attacks
+DEFAULT_THREADS = 800  # Set the default number of threads to use for attacks
